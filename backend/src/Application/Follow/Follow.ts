a657a6a28f4de2bf057068b97fb27447b98d5eab
Bartek Kurpanik <bartekkur1@gmail.com>
2024-05-12 17:38:53 +0200

    Added follow handler instance

---
+++ backend/src/Application/Follow/Follow.ts
@@ -0,0 +1,17 @@
+import { getConsoleLogger } from '../../Util/logger';
+import { Logger } from '../../Util/types';
+
+class Follow {
+
+  private logger: Logger = getConsoleLogger('Follow');
+
+  constructor() { }
+
+
+  followUser(followerId: number, followingId: number) {
+    this.logger.info(`User ${followerId} following ${followingId}`);
+  }
+
+};
+
+export default Follow;
